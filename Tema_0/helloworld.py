c591a68f3cff6a40bb389bdfd774f307a65519c2
Roberto Morano García <roberto.morano@info2>
2025-09-17 10:31:20 +0200

    create

---
+++ Tema_0/helloworld.py
@@ -0,0 +1,18 @@
+print("holou moto")
+nombre= input("What is your name? ")
+print("hola", nombre * (10**6))
+aweno = "12345678901234567890"
+print(aweno[2:9:3])
+
+# Hola soy un comentario mu bonico
+
+a = 1
+b = 2
+
+if a == b:
+    print("a es igual a b")
+elif a > b:
+    print("a es mayor que b")
+else:
+    print("a es menor que b")
+
