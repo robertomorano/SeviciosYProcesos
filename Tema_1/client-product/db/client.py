14c6645c1c4adb57ebc9fc7b210aaab9a11e0eb9
robertomorano <roberto.morano@iesnervion.es>
2025-12-17 09:36:37 +0100

    holamundo

--- Tema_1/client-product/db/client.py
+++ Tema_1/client-product/db/client.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 #db_client = MongoClient()
 
-db_client = MongoClient("mongodb+srv://robertomorano_db_user:MxUWgWx68MEKHxfT@clustertest.vcuimis.mongodb.net/")
+db_client = MongoClient("mongodb+srv://robertomorano_db_user:MxUWgWx68MEKHxfT@clustertest.vcuimis.mongodb.net/?appName=ClusterTest")
